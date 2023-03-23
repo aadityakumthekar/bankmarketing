e6b1ac02134e57c330d60601fe906fc9cebb28d9
aadityakumthekar <aditya_kumthekar@persistent.com>
2023-03-23 22:11:19 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import pickle
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def webpage():
+    return render_template('webpage.html')
+
+def predict():
+    prediction = model.predict([request.form.get('age','job','duration','')])
+    output = (prediction)
+    print(output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+def get_function():
+    age = request.form.get('age')
+    
+
+
+
+
+
+# Create a form in your Flask application that allows users to input their data. This form can be created using HTML and can be rendered using Flask's render_template function.
+
+# Once the user submits the form, you can use Flask's request object to retrieve the data that was inputted. For example, if the form has fields for name and age, you can retrieve the values using the following code:
+
+# python
